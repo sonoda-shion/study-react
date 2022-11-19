356f2d910d7cd86f744f3715378bafa0bb76536a
sonoda-shion <xcgikdue2z@i.softbank.jp>
2022-11-19 14:39:40 +0900

    ♻️ new component : Headline

---
+++ components/headline.js
@@ -0,0 +1,17 @@
+import styles from '../styles/Home.module.css'
+
+
+export function Headline({title,page}) {
+  return (
+    <>
+        <h1 className={styles.title}>
+          {title} Page
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/{page}.js</code>
+        </p>
+    </>
+  )
+}
